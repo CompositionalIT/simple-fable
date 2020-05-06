ca8099d37c8e833543dfca8f8b79664bceaacec5
isaacabraham <isaac.abraham@gmail.com>
2020-05-07 00:13:32 +0200

    Add npm package

--- src/App.fs
+++ src/App.fs
@@ -15,3 +15,42 @@ myButton.onclick <- fun _ ->
 
     if count > 5 then
         window.alert (sprintf "%O: Please stop clicking me!" System.DateTime.UtcNow)
+
+open Fable.Core
+open Fable.Core.JsInterop
+open Fable.Core.Util
+// Simple reference of an module from an NPM package, upper-case
+// See https://www.npmjs.com/package/upper-case
+// Notice that F# correctly infers the function uppercase to be
+// string -> string from the usage two lines later!
+let uppercase = import "upperCase" "upper-case"
+let myText = document.getElementById "my-text"
+myText.innerText <- uppercase "Hello"
+
+// Or use some lower-level alternative like this:
+[<Emit "upperCase($0)">]
+let foo (x:string) : string= jsNative
+console.log (foo "blah de blah") // check the console output...
+
+// You can do some pretty crazy stuff with Fable mixing JS's super
+// weak runtime with F# on top!
+[<Emit "$0 + $1">]
+let addThroughJs (x: int) (y: int) = jsNative
+console.log (addThroughJs 10 5)
+
+// Look at this. We can add a number and a string together through
+// JS. And then add another number to it. And it works, more or less!
+
+[<Emit "$0 + $1">]
+let addJsCrazy (x: int) (y: string) : int = jsNative
+console.log (addJsCrazy 10 "99" + 20)
+
+// Alternatively, you can make an interface that maps to the
+// bits of the module that you want to expose in F#.
+type IUpperCaseModule =
+    abstract upperCase : string -> string
+    abstract localeUpperCase : string * string -> string
+
+let ucModule = importAll<IUpperCaseModule> "upper-case"
+console.log (ucModule.upperCase "hello")
+console.log (ucModule.localeUpperCase("hello", "fr"))
